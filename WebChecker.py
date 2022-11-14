32db3c5ab1bb8fe899deaac776c4a00394d9c45a
cline99 <54473001+cline99@users.noreply.github.com>
2022-11-14 23:05:37 +0530

    fix

--- WebChecker.py
+++ WebChecker.py
@@ -11,14 +11,12 @@ parser.add_argument("-u", "--url", help="Specify the URL ypu want to Check")
 parser.add_argument("-b", "--brute", help="Attempt to bruteforce interesting directories")
 args = parser.parse_args()
 
-########################  GLOBALS  ########################
 
-CMS_error = True
-WP = False
-JOOM = False
-MAG = False
-DRUP = False
-confidence = 0
+no_cms_flag = True
+wordpress_flg = False
+joomla_flag = False
+drupal_flag = False
+certainty = 0
 url = ''
 url_request = ''
 url_socket = ''
@@ -30,7 +28,6 @@ __          __  _      _____ _               _               __   ___
   \ \/  \/ / _ \ '_ \| |    | '_ \ / _ \/ __| |/ / _ \ '__|  | || | | |
    \  /\  /  __/ |_) | |____| | | |  __/ (__|   <  __/ |     | || |_| |
     \/  \/ \___|_.__/ \_____|_| |_|\___|\___|_|\_\___|_|     |_(_)___/ 
-
                             by mihnaemanolache
                     https://github.com/mihneamanolache/
     ''')
@@ -38,14 +35,12 @@ term_size = os.get_terminal_size()
 COMMAND = 'clear'
 if os.name in ('nt', 'dos'):  
     COMMAND = 'cls'
-########################  RUNNING  ########################
 
 os.system(COMMAND)
 cprint(banner, 'blue')
 cprint('-'*term_size.columns, 'blue')
 
 if args.url is None:
-        # Get the input from the user
         url = input('Site to check: ')
         cprint('-'*term_size.columns, 'blue')
 else:
@@ -64,175 +59,131 @@ try:
     
     r = requests.get(url_request, allow_redirects=True, headers=user_agent)
 
-    IP_addres = socket.gethostbyname(url_socket)
+    IP = socket.gethostbyname(url_socket)
 
-    cprint(f'[*] Checking: {url}...', 'yellow')
-    cprint(f'[!] {url} seems to be online', 'green')
-    print('')
+    #cprint(f'[*] Checking: {url}...', 'yellow')
+
+    print('[-->] Fetching Details of {}'.format(url))
+
+    #cprint(f'', 'green')
+
+    print('[!] {} seems to be online'.format(url))
     
-    cprint("[*] Attempting to identify potential CMS...", 'yellow')
+    print("[*] Attempting to identify potential CMS...", 'yellow')
 
 
-    # ------ CHECKING WORDPRESS ------ #
-    cprint("[!] Running WordPress scans...", 'magenta')
+    print("[!] Running WordPress scans...")
     
-    wp_url = requests.get(url_request + '/wp-login.php', allow_redirects=True, headers=user_agent)
-    if wp_url.status_code == 200 and "user_login" in wp_url.text and "404" not in wp_url.text:
+    wordpress_flg_url = requests.get(url_request + '/wp-login.php', allow_redirects=True, headers=user_agent)
+    if wordpress_flg_url.status_code == 200 and "user_login" in wordpress_flg_url.text and "404" not in wordpress_flg_url.text:
         cprint(f"    [+] WordPress login page available at {url_request}/wp-login.php", 'green')
-        CMS_error = False
-        WP = True
-        confidence += 25
+        no_cms_flag = False
+        wordpress_flg = True
+        certainty += 25
     else:
-        cprint(f"    [-] WordPress login page not available", 'red')
+        print("    [-] WordPress login page not available")
     
-    wp_url = requests.get(url_request + '/wp-admin/upgrade.php', allow_redirects=False, headers=user_agent)
-    if wp_url.status_code == 200 and "404" not in wp_url.text:
-        CMS_error = False
-        WP = True
-        cprint(f"    [+] WP-Admin/upgrade.php page available at {url_request}/wp-admin/upgrade.php", 'green')
-        if confidence == 100:
+    wordpress_flg_url = requests.get(url_request + '/wp-admin/upgrade.php', allow_redirects=False, headers=user_agent)
+    if wordpress_flg_url.status_code == 200 and "404" not in wordpress_flg_url.text:
+        no_cms_flag = False
+        wordpress_flg = True
+        print("    [+] WP-Admin/upgrade.php page available at {}/wp-admin/upgrade.php".format(url_request))
+        if certainty == 100:
             pass
         else:    
-            confidence += 25
+            certainty += 25
     else:
-        cprint(f"    [-] WP-Admin/upgrade.php page not available", 'red')
+        print("    [-] WP-Admin/upgrade.php page not available")
     
-    wp_url = requests.get(url_request + '/wp-json/wp/v2/', allow_redirects=False, headers=user_agent)
-    if wp_url.status_code == 200 and "404" not in wp_url.text:
-        CMS_error = False
-        WP = True
-        cprint(f"    [+] WP API available at {url_request}/wp-json/wp/v2/", 'green')
-        if confidence == 100:
+    wordpress_flg_url = requests.get(url_request + '/wp-json/wp/v2/', allow_redirects=False, headers=user_agent)
+    if wordpress_flg_url.status_code == 200 and "404" not in wordpress_flg_url.text:
+        no_cms_flag = False
+        wordpress_flg = True
+        print("    [+] WP API available at {}/wp-json/wp/v2/".format(url_request))
+        if certainty == 100:
             pass
         else:    
-            confidence += 25
+            certainty += 25
     else:
-        cprint(f"    [-] WP API not available", 'red')
+        print(f"    [-] WP API not available")
     
-    wp_url = requests.get(url_request + '/robots.txt', allow_redirects=True, headers=user_agent)
-    if wp_url.status_code == 200 and "wp-admin" in wp_url.text:
-        CMS_error = False
-        WP = True
-        cprint(f"    [+] Robots.txt fount at {url_request}/robots.txt containing 'wp_admin'", 'green')
-        if confidence == 100:
+    wordpress_flg_url = requests.get(url_request + '/robots.txt', allow_redirects=True, headers=user_agent)
+    if wordpress_flg_url.status_code == 200 and "wp-admin" in wordpress_flg_url.text:
+        no_cms_flag = False
+        wordpress_flg = True
+        print("    [+] Robots.txt fount at {}/robots.txt containing 'wp_admin'".format(url_request))
+        if certainty == 100:
             pass
         else:    
-            confidence += 25
+            certainty += 25
     else:
-        cprint(f"    [-] Robots.txt not found", 'red')
+        print("    [-] Robots.txt not found")
     
-    # ------ CHECKING JOOMLA ------ #
-    cprint("[!] Running Joomla scans...", 'magenta')
+    print("[!] Running Joomla scans...")
 
-    joom_url = requests.get(url_request + '/administrator/')
-    if joom_url.status_code == 200 and "mod-login-username" in joom_url.text and "404" not in joom_url.text:
-        CMS_error = False
-        JOOM = True
-        cprint(f"    [+] {url_request} seems to be running on Joomla", 'green')
-        confidence += 100
+    joomla_flag_url = requests.get(url_request + '/administrator/')
+    if joomla_flag_url.status_code == 200 and "mod-login-username" in joomla_flag_url.text and "404" not in joomla_flag_url.text:
+        no_cms_flag = False
+        joomla_flag = True
+        print("    [+] {} seems to be running on Joomla".format(url_request))
+        certainty += 100
     else:
-        cprint(f"    [-] {url_request} doesn't seem to be running on Joomla", 'red')
+        print("    [-] {} doesn't seem to be running on Joomla".format(url_request))
+   
+    print("[!] Running Drupal scans...")
 
-    # ------ CHECKING MAGENTO ------ #
-    cprint("[!] Running Magento scans...", 'magenta')
-
-    mag_url = requests.get(url_request + '/index.php', allow_redirects=False)
-    if mag_url.status_code == 200 and '/mage/' in mag_url.text or 'magento' in mag_url.text:
-        CMS_error = False
-        MAG = True
-        confidence += 25
-        cprint(f"    [+] Magento strings detected.", 'green')
+    drupal_flag_url = requests.get(url_request + '/readme.txt')
+    if drupal_flag_url.status_code == 200 and 'drupal' in drupal_flag_url.text and '404' not in drupal_flag_url.text:
+        no_cms_flag = False
+        drupal_flag = True
+        certainty += 33
+        print("    [+] Drupal Readme.txt detected at {}/readme.txt".format(url_request))
     else:
-        cprint(f"    [-] No Magento strings detected", 'red')
-        
-    mag_url = requests.get(url_request + '/index.php/admin/', allow_redirects=False)
-    if mag_url.status_code == 200 and 'login' in mag_url.text and "404" not in mag_url.text:
-        CMS_error = False
-        MAG = True   
-        confidence += 25     
-        cprint(f"    [+] Potential Magento admin login at {url_request}/index.php/admin/", 'green')
-    else:
-        cprint(f"    [-] {url_request}/index.php/admin/ not available", 'red')
-    
-    mag_url = requests.get(url_request + '/RELEASE_NOTES.txt')
-    if mag_url.status_code == 200 and 'magento' in mag_url.text:
-        CMS_error = False
-        MAG = True   
-        confidence += 25     
-        cprint(f"    [+] Magento Release_Notes.txt detected at {url_request}/RELEASE_NOTES.txt", 'green')
-    else:
-        cprint(f"    [-] Magento Release_Notes.txt not detected", 'red')
-    
-    mag_url = requests.get(url_request + '/js/mage/cookies.js')
-    if mag_url.status_code == 200 and "404" not in mag_url.text:
-        CMS_error = False
-        MAG = True    
-        confidence += 25    
-        cprint(f"    [+] Magento cookies.js detected at {url_request}/js/mage/cookies.jst", 'green')
-    else:
-        cprint(f"    [-] Magento cookies.js not detected", 'red') 
+        print("    [-] Drupal Readme.txt not detected")
 
-    # ------ CHECKING DRUPAL ------ #
-    cprint("[!] Running Drupal scans...", 'magenta')
-
-    drup_url = requests.get(url_request + '/readme.txt')
-    if drup_url.status_code == 200 and 'drupal' in drup_url.text and '404' not in drup_url.text:
-        CMS_error = False
-        DRUP = True
-        confidence += 33
-        cprint(f"    [+] Drupal Readme.txt detected at {url_request}/readme.txt", 'green')
+    drupal_flag_url = requests.get(url_request)
+    if drupal_flag_url.status_code == 200 and 'name="Generator" content="Drupal' in drupal_flag_url.text:
+        no_cms_flag = False
+        drupal_flag = True
+        certainty += 33
+        print("    [+] Drupal strings detected.")
     else:
-        cprint(f"    [-] Drupal Readme.txt not detected", 'red')
+        print("    [-] No Drupal string detected")
 
-    drup_url = requests.get(url_request)
-    if drup_url.status_code == 200 and 'name="Generator" content="Drupal' in drup_url.text:
-        CMS_error = False
-        DRUP = True
-        confidence += 33
-        cprint(f"    [+] Drupal strings detected.", 'green')
+    drupal_flag_url = requests.get(url_request + '/modules/README.txt')
+    if drupal_flag_url.status_code == 200 and 'drupal' in drupal_flag_url.text and '404' not in drupal_flag_url.text:
+        no_cms_flag = False
+        drupal_flag = True
+        certainty += 33
+        cprint(f"    [+] Drupal modules detected at {}/modules/README.txt".format(url_request))
     else:
-        cprint(f"    [-] No Drupal string detected", 'red')
+        print(f"    [-] No Drupal modules detected")
 
-    drup_url = requests.get(url_request + '/modules/README.txt')
-    if drup_url.status_code == 200 and 'drupal' in drup_url.text and '404' not in drup_url.text:
-        CMS_error = False
-        DRUP = True
-        confidence += 33
-        cprint(f"    [+] Drupal modules detected at {url_request}/modules/README.txt", 'green')
-    else:
-        cprint(f"    [-] No Drupal modules detected", 'red')
+    print('-'*term_size.columns)
 
-    print('')
-    cprint('-'*term_size.columns, 'blue')
-
-    # ------ DISPLAY APP OVERVIEW ------ #
     status = r.status_code
     server_type = r.headers['Server']
     cprint('App overview:', 'magenta')
     cprint(f'   [+] {url} is available', 'green')
-    if CMS_error:
+    if no_cms_flag:
         cprint(f"   [!] {url} doesn't seem to run any known CMS", 'yellow')
-    elif WP:
-        cprint(f"   [+] {url} seems to be running WordPress. [Confidence: {confidence}%]", 'green')
-    elif JOOM:
-        cprint(f"   [+] {url} seems to be running Joomla. [Confidence: {confidence}%]", 'green')
-    elif MAG:
-        cprint(f"   [+] {url} seems to be running Magento. [Confidence: {confidence}%]", 'green')
-    elif DRUP:
-        cprint(f"   [+] {url} seems to be running Drupal. [Confidence: {confidence}%]", 'green')
+    elif wordpress_flg:
+        cprint(f"   [+] {url} seems to be running WordPress. [certainty: {certainty}%]", 'green')
+    elif joomla_flag:
+        cprint(f"   [+] {url} seems to be running Joomla. [certainty: {certainty}%]", 'green')
+    elif drupal_flag:
+        cprint(f"   [+] {url} seems to be running Drupal. [certainty: {certainty}%]", 'green')
     cprint(f'   [+] HTTP Status:    {status}', 'green')
     cprint(f'   [+] Server type:    {server_type}', 'green')
-    cprint(f'   [+] Server IP:      {IP_addres}', 'green')
+    cprint(f'   [+] Server IP:      {IP}', 'green')
     print('')
     cprint('-'*term_size.columns, 'blue')
     
-     # ------ DISPLAY HEADERS ------ #
     cprint('Detailed HTTP header report:', 'magenta')
-    for i in r.headers: # -> Printare Headers pe linii (prettyprint)
+    for i in r.headers: 
         cprint(f"   [*] {i}: {r.headers[i]}", 'cyan') 
     print('')
 
-# ------ ERROR: INVALID URL ------ #
 except requests.exceptions.RequestException as e: 
     cprint(f'[!] Checking: {url}...', 'yellow')
     print('')
@@ -241,7 +192,6 @@ except requests.exceptions.RequestException as e:
     cprint('ERROR MESSAGE:', 'yellow', end=' ')
     raise SystemExit(e)
 
-# ------ BRUTE FORCING DIRECTORIES ------ #
 if args.brute:
     cprint('-'*term_size.columns, 'blue')
     cprint('Bruteforcing interesting directories:', 'magenta')
